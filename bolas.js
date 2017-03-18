dbfc6fa2f276feb0d947d729e4ed4676f0c4a581
fabriciojunior7 <fabriciovcjr@hotmail.com>
2017-03-18 03:10:41 -0300

    Add files via upload

--- bolas.js
+++ bolas.js
@@ -1,15 +1,17 @@
-function Bola(x, y, largura, altura){
+function Bola(x, y, largura, altura, velocidadeXInicial){
 	//Atributos
 	this.x = x;
 	this.y = y;
 	this.largura = largura;
 	this.altura = altura;
-	this.velocidadeX = 5.0;
+	this.velocidadeXInicial = velocidadeXInicial;
+	this.velocidadeX = this.velocidadeXInicial;
 	this.velocidadeXMAX = 20.0
 	this.velocidadeY = 0.0;
 	this.r = 255;
 	this.g = 255;
 	this.b = 255;
+	this.movimentar = false;
 
 	//Metodos
 	this.desenhar = function(){
@@ -18,11 +20,18 @@ function Bola(x, y, largura, altura){
 		rect(this.x, this.y, this.largura, this.altura);
 	};
 
-	this.atualizarPosicao = function(){
-		this.x += this.velocidadeX;
+	this.atualizarPosicao = function(altura){
+		if(this.movimentar == true){
+			this.x += this.velocidadeX;
+			this.y += this.velocidadeY;
+		}
+
+		if(this.y < 0 || this.y > altura-this.altura){
+			this.velocidadeY *= -1;
+		}
 	};
 
-	this.bateu = function() {
+	this.bateu = function(y) {
 		//Eixo X
 		this.velocidadeX *= -1;
 		if(this.velocidadeX > 0 && this.velocidadeX < this.velocidadeXMAX){
@@ -33,8 +42,8 @@ function Bola(x, y, largura, altura){
 		}
 
 		//Eixo Y
-		
-	}
+		this.velocidadeY = y*0.2;
+	};
 
 
 
--- main.js
+++ main.js
@@ -8,10 +8,10 @@ function setup(){
 	tela = createCanvas(largura, altura);
 
 	jogador1 = new Jogador(50, (altura/2 - 35), 12, 70, "W", "S", 0, 255, 0);
-	jogador2 = new Jogador(largura-60, (altura/2 - 35), 12, 70, "&", "(", 255, 0, 0);
-	bola = new Bola(largura/2, altura/2, 10, 10);
+	jogador2 = new Jogador((largura - 60), (altura/2 - 35), 12, 70, "&", "(", 255, 0, 0);
+	bola = new Bola((largura/2 - 5), (altura/2 - 5), 10, 10, int(largura*0.007));
 	frames = 60;
-	textSize(32);
+	textSize(38);
 }
 
 function draw(){
@@ -22,18 +22,47 @@ function draw(){
 	//Atualizar Posicoes
 	jogador1.atualizarPosicao();
 	jogador2.atualizarPosicao();
-	bola.atualizarPosicao();
+	bola.atualizarPosicao(altura);
+	if(bola.x < 0){
+		jogador2.score++;
+		bola.movimentar = false;
+		bola.x = largura/2 - 5;
+		bola.y = altura/2 - 5;
+		bola.velocidadeX = bola.velocidadeXInicial;
+		bola.velocidadeY = 0;
+		if(jogador2.score >= 5){
+			fim(2);
+		}
+	}
+	else if(bola.x > largura){
+		jogador1.score++;
+		bola.movimentar = false;
+		bola.x = largura/2 - 5;
+		bola.y = altura/2 - 5;
+		bola.velocidadeX = -bola.velocidadeXInicial;
+		bola.velocidadeY = 0;
+		if(jogador1.score >= 5){
+			fim(1);
+		}
+	}
 	//Desenhar
 	jogador1.desenhar();
 	jogador2.desenhar();
 	bola.desenhar();
-	text(bola.velocidadeY, 50, 50);
+	//text(bola.velocidadeX, 50, 50);
+	fill(jogador1.r, jogador1.g, jogador1.b);
+	text(jogador1.score, (largura/2 - largura*0.055), 40);
+	fill(jogador2.r, jogador2.g, jogador2.b);
+	text(jogador2.score, (largura/2 + largura*0.03), 40);
 
 	bolaP1 = collideRectRect(jogador1.x, jogador1.y, jogador1.largura, jogador1.altura, bola.x, bola.y, bola.largura, bola.altura);
 	bolaP2 = collideRectRect(jogador2.x, jogador2.y, jogador2.largura, jogador2.altura, bola.x, bola.y, bola.largura, bola.altura);
-	if(bolaP1 == true || bolaP2 == true){
-		bola.bateu();
-		print("oi");
+
+	if(bolaP1 == true){
+		bola.bateu((bola.y+(bola.altura/2)) - (jogador1.y+(jogador1.altura/2)));
+	}
+	else if(bolaP2 == true){
+		bola.bateu((bola.y+(bola.altura/2)) - (jogador2.y+(jogador2.altura/2)));
 	}
 }
 
@@ -45,4 +74,23 @@ function keyPressed(){
 function keyReleased(){
 	jogador1.botaoSolto(key);
 	jogador2.botaoSolto(key);
+	if(key == " "){
+		bola.movimentar = true;
+	}
+}
+
+function fim(vencedor) {
+	if(vencedor == 1){
+		background(jogador1.r, jogador1.g, jogador1.b);
+		fill(255);
+		textSize(50);
+		text("Verde Venceu!", largura/2-150, altura/2);
+	}
+	else if(vencedor == 2){
+		background(jogador2.r, jogador2.g, jogador2.b);
+		fill(255);
+		textSize(50);
+		text("Vermelho Venceu!", largura/2-190, altura/2);
+	}
+	noLoop();
 }
