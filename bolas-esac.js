0d1413fc78c555142b73438aa16098e7efb54f93
fabriciojunior7 <fabriciovcjr@hotmail.com>
2017-03-21 00:28:35 -0300

    Add files via upload

---
+++ bolas-esac.js
@@ -0,0 +1,60 @@
+function Bola(x, y, largura, altura, velocidadeXInicial){
+	//Atributos
+	this.x = x;
+	this.y = y;
+	this.largura = largura;
+	this.altura = altura;
+	this.velocidadeXInicial = velocidadeXInicial;
+	this.velocidadeX = this.velocidadeXInicial;
+	this.velocidadeXMAX = 30.0;
+	this.velocidadeY = 0.0;
+	this.r = 255;
+	this.g = 255;
+	this.b = 255;
+	this.movimentar = false;
+	this.crescerVelocidadeX = 0.75;
+
+	//Metodos
+	this.desenhar = function(){
+		noStroke();
+		fill(this.r, this.g, this.b);
+		rect(this.x, this.y, this.largura, this.altura);
+	};
+
+	this.atualizarPosicao = function(altura){
+		if(this.movimentar == true){
+			this.x += this.velocidadeX;
+			this.y += this.velocidadeY;
+		}
+
+		if(this.y < 0 || this.y > altura-this.altura){
+			this.velocidadeY *= -1;
+			if(this.y <= 0){
+				this.y += 5;
+			}
+			else if(this.y > altura-this.altura){
+				this.y -= 5;
+			}
+		}
+	};
+
+	this.bateu = function(y) {
+		//Eixo X
+		this.velocidadeX *= -1;
+		if(this.velocidadeX > 0 && this.velocidadeX < this.velocidadeXMAX){
+			this.velocidadeX += this.crescerVelocidadeX;
+		}
+		else if(this.velocidadeX < 0 && this.velocidadeX > -this.velocidadeXMAX){
+			this.velocidadeX -= this.crescerVelocidadeX;
+		}
+
+		//Eixo Y
+		this.velocidadeY = y*0.2;
+	};
+
+
+
+
+
+
+}
--- main-esac.js
+++ main-esac.js
@@ -12,6 +12,7 @@ var start;
 var hit;
 var aplausos;
 var errou, peido1, peido2, peido3, peido4, se_vc_chegar, hector, ta_fedendo, pode_nao, adonai;
+var mercin2, prazer, pato, neuma, jorge_roberto;
 var somAleatorio;
 
 function preload(){
@@ -29,6 +30,12 @@ function preload(){
 	pode_nao = loadSound("sons/pode-nao.mp3");
 	adonai = loadSound("sons/adonai.mp3");
 	aplausos = loadSound("sons/aplausos.mp3");
+	mercin2 = loadSound("sons/mercin2.mp3");
+	prazer = loadSound("sons/prazer.mp3");
+	pato = loadSound("sons/pato.mp3");
+	neuma = loadSound("sons/neuma.mp3");
+	jorge_roberto = loadSound("sons/jorge-roberto.mp3");
+	bater_ponto = loadSound("sons/bater-ponto.mp3");
 }
 
 function setup(){
@@ -36,11 +43,17 @@ function setup(){
 	altura = windowHeight;
 	tela = createCanvas(largura, altura);
 
-	iniciar.play();
+	somAleatorio = int(random(1, 3));
+	if(somAleatorio == 1){
+		iniciar.play();
+	}
+	else if(somAleatorio == 2){
+		prazer.play();
+	}
 
-	jogador1 = new Jogador(50, (altura/2 - 35), 15, 70, "W", "S", 0, 255, 0);
-	jogador2 = new Jogador((largura - 60), (altura/2 - 35), 15, 70, "&", "(", 255, 0, 0);
-	bola = new Bola((largura/2 - 5), (altura/2 - 5), 10, 10, int(largura*0.0075));
+	jogador1 = new Jogador(40, (altura/2 - 35), 20, 70, "W", "S", 0, 255, 0);
+	jogador2 = new Jogador((largura - 60), (altura/2 - 35), 20, 70, "&", "(", 255, 0, 0);
+	bola = new Bola((largura/2 - 5), (altura/2 - 5), 10, 10, 20);
 	frames = 60;
 	score1Y = 40;
 	score2Y = 40;
@@ -98,16 +111,23 @@ function draw(){
 	fill(jogador2.r, jogador2.g, jogador2.b);
 	score2 = text(jogador2.score, (largura/2 + largura*0.03), score2Y);
 
+	//Colisao
 	bolaP1 = collideRectRect(jogador1.x, jogador1.y, jogador1.largura, jogador1.altura, bola.x, bola.y, bola.largura, bola.altura);
 	bolaP2 = collideRectRect(jogador2.x, jogador2.y, jogador2.largura, jogador2.altura, bola.x, bola.y, bola.largura, bola.altura);
 
 	if(bolaP1 == true){
+		bola.x = jogador1.x + jogador1.largura + bola.largura + 5;
 		bola.bateu((bola.y+(bola.altura/2)) - (jogador1.y+(jogador1.altura/2)));
 		hit.play();
 	}
 	else if(bolaP2 == true){
+		bola.x = jogador2.x - bola.largura - 5;
 		bola.bateu((bola.y+(bola.altura/2)) - (jogador2.y+(jogador2.altura/2)));
 		hit.play();
+	}
+
+	if(frameCount % 300 == 0 && bola.x > largura*0.4 && bola.x < largura*0.6 && bola.movimentar == true){
+		perdeuSom();
 	}
 
 	//if(frameCount % 480 == 0 && bola.movimentar == true){
@@ -129,11 +149,12 @@ function keyReleased(){
 	if(key == " " && bola.movimentar == false){
 		start.play();
 		bola.movimentar = true;
+		bola.velocidadeY = random(-6, 6);
 	}
 }
 
 function perdeuSom(){
-	somAleatorio = int(random(1, 11));
+	somAleatorio = int(random(1, 15));
 	if(somAleatorio == 1 || somAleatorio == 2){
 		errou.play();
 	}
@@ -161,6 +182,18 @@ function perdeuSom(){
 	else if(somAleatorio == 10){
 		adonai.play();
 	}
+	else if(somAleatorio == 11){
+		mercin2.play();
+	}
+	else if(somAleatorio == 12){
+		pato.play();
+	}
+	else if(somAleatorio == 13){
+		neuma.play();
+	}
+	else if(somAleatorio == 14){
+		jorge_roberto.play();
+	}
 }
 
 function fim(vencedor) {
@@ -171,14 +204,19 @@ function fim(vencedor) {
 	score2Y = 2*altura;
 
 	//Som
-	somAleatorio = int(random(1, 3));
+	somAleatorio = int(random(1, 5));
 	if(somAleatorio == 1){
 		aplausos.setVolume(0.2);
 		aplausos.play();
 	}
 	else if(somAleatorio == 2){
-
 		se_vc_chegar.play();
+	}
+	else if(somAleatorio == 3){
+		bater_ponto.play();
+	}
+	else if(somAleatorio == 4){
+		jorge_roberto.play();
 	}
 
 	if(vencedor == 1){
